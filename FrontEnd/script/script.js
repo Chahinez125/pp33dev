fc0375ea671fd41615241f04de437b79bdb8544e
Chahinez125 <chahinez.semmar@yahoo.com>
2024-04-28 22:37:47 +0200

    creation des buttons udapt css btn

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,20 +1,26 @@
 // je récupere les travaux depui le backend 
-
-
 //déclare une variable
 const gallery = document.querySelector(".gallery")
+const container = document.querySelector(".container-filtres")
 
 
 //fonction qui va recupérer mes travaux getWork j'ai async pr lui dire atten dans cette fontion tu dois attens avant de lire mon code 
 //await sa veux tu attend que le fetch dois finit avant que tu l'enregistre dans response
 async function getWorks(){
     const response = await fetch("http://localhost:5678/api/works")
-    //a chaque foi j'appel getWork je veux quelle me retur await.json notre tableau
+    //a chaque foi j'appel getWork je veux quelle me retur await.json dans notre tableau
     return await response.json();
 
 }
 
 getWorks();
+/*Récupere le tableaux de gatégorie***/
+async function getCategory(){
+    const response = await fetch("http://localhost:5678/api/categories")
+    return await response.json();
+    
+    }
+     getCategory();
 //affichage des works dans le dom
 async function updatWorks(){
     //j'enregitre 
@@ -31,3 +37,19 @@ figcaption.textContent = work.title;
     });
 }
 updatWorks();
+
+/***cette function pour afficher les bouttons par catégories */ 
+//récuperer le tableau des Cat
+async function displayCategoriesBouttons(){
+    const catégorys = await getCategory();
+    console.log(catégorys);catégorys.forEach(catégory => { //crée une varible pr appeler btn
+        const btn = document.createElement("button");
+        btn.textContent= catégory.name;
+        btn.id = catégory.id;
+        container.appendChild(btn);
+
+    });
+
+
+}
+displayCategoriesBouttons();
