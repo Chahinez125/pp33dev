1484208fb507b0b7023c8c41e72b177442551e9a
Chahinez125 <chahinez.semmar@yahoo.com>
2024-04-29 18:08:09 +0200

    affichage et try les work par cat

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -6,7 +6,7 @@ const container = document.querySelector(".container-filtres")
 
 //fonction qui va recupérer mes travaux getWork j'ai async pr lui dire atten dans cette fontion tu dois attens avant de lire mon code 
 //await sa veux tu attend que le fetch dois finit avant que tu l'enregistre dans response
-async function getWorks(){
+async function getWorks() {
     const response = await fetch("http://localhost:5678/api/works")
     //a chaque foi j'appel getWork je veux quelle me retur await.json dans notre tableau
     return await response.json();
@@ -14,66 +14,79 @@ async function getWorks(){
 }
 
 getWorks();
-/*Récupere le tableaux de gatégorie***/
-async function getCategory(){
-    const response = await fetch("http://localhost:5678/api/categories")
-    return await response.json();
-    
-    }
-     getCategory();
+
 //affichage des works dans le dom
-async function updatWorks(){
+
+async function displayWorks() {
     //j'enregitre 
     const arrayWorks = await getWorks();
     arrayWorks.forEach(work => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figure");
-img.src = work.imageUrl;
-img.alt = work.title;
-        figure.appendChild(img);
-  figure.appendChild(figcaption);
-  gallery.appendChild(figure); 
-    });
-}
-updatWorks();
+        createWork(work);
+        });
+        }
+        displayWorks();
+        function createWork(work){
+            const figure = document.createElement("figure");
+            const img = document.createElement("img");
+            const figcaption = document.createElement("figcaption");
+            img.src = work.imageUrl;
+            figcaption.textContent = work.title;
+           
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        }
+        
 
-/***cette function pour afficher les bouttons par catégories */ 
+/***cette function pour afficher les bouttons par catégories */
+/*Récupere le tableaux de gatégorie***/
+async function getCategory() {
+    const response = await fetch("http://localhost:5678/api/categories")
+    return await response.json();
+
+}
+getCategory();
 //récuperer le tableau des Cat
-async function displayCategoriesBouttons(){
+async function displayCategoriesBouttons() {
     const catégorys = await getCategory();
-    console.log(catégorys);catégorys.forEach(catégory => { //crée une varible pr appeler btn
+    console.log(catégorys); catégorys.forEach(catégory => { //crée une varible pr appeler btn
         const btn = document.createElement("button");
-        btn.classList.add(".container-filters");
-        btn.textContent= catégory.name;
+        //btn.classList.add(".container-filters");
+        btn.textContent = catégory.name;
         btn.id = catégory.id;
         container.appendChild(btn);
 
     });
-
-
 }
 displayCategoriesBouttons();
 //filtré au clique sur le btn par catégories
 async function filterCategory() {
     //je vais récupere tableaux qui contien tous les travaux
- const works = await getWorks();
- console.log(works);
- const buttons = document.querySelectorAll("button");
- buttons.forEach(button => {
-    //pr chaque btn en le selectionne et en va ecouter l'evenement qui va jouet c'est ta dire par ex a chaque foi je clique sur boutton ya l'affichage de l'id
-    button.addEventListener("click", (e)=>{
-       const btnId = e.target.id;
-        console.log(btnId);
-     //maintenant je vaux que a chaqque click avant de genere des autre image je veux qu'il me les supprime avant de me donner des nouvelle resultat 
-      gallery.innerHTML = "";
-    })
+    const workss = await getWorks();
+    console.log(workss);
+    const buttons = document.querySelectorAll(".container-filtres button");
+    buttons.forEach(button => {
+        //pr chaque btn en le selectionne et en va ecouter l'evenement qui va jouet c'est ta dire par ex a chaque foi je clique sur boutton ya l'affichage de l'id
+        button.addEventListener("click", (e) => {
+            const btnId = e.target.id; //a chaque clique en à une inf qui nous donne l'id de cat
 
-    
+            //maintenant je vaux que a chaqque click avant de genere des autre image je veux qu'il me les supprime avant de me donner des nouvelle resultat 
+            //sa veux dire actualiser la gallerie  je dit a ma gallery de temettre a 0 donne 0 resultat
+            gallery.innerHTML = "";
+            //la je vais faire un conditions si mon id et défferent de 0 
+            if (btnId !== "0"){
+                const tryCategory = workss.filter((work) => {
+                    return work.categoryId == btnId;
+                });
+                tryCategory.forEach((work) => {
+                    createWork(work);
+                });
+            }else{
+                displayWorks();
+            }
+            console.log(btnId);
 
- });
- 
+        });
+    });
 }
 filterCategory();
-
-
