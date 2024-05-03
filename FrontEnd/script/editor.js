91d6e815bef66ce8f51567c249d1b7a7752b70e8
Chahinez125 <112867826+Chahinez125@users.noreply.github.com>
2024-05-04 00:27:18 +0200

    Changemen  text login en logout et supp filter

--- FrontEnd/script/editor.js
+++ FrontEnd/script/editor.js
@@ -1,11 +1,31 @@
 
-function editOR() {
+/*function editOR() {
     const userEdit = window.localStorage.getItem('token');
     if (userEdit) { // Vérifie si un jeton est présent dans le stockage local
         const headBar = document.querySelector('.HeadBar');
         headBar.style.display = 'flex'; // Affiche la barre d'en-tête
         
     }
-  }
+  }*/
+export function editMode(){
+    //modif bar edit html/css
+    const adminconnecter = window.localStorage.getItem('token');
+    if(adminconnecter){
+        //afficher barre editor
+        const headBar = document.querySelector('.HeadBar');
+        headBar.style.display = 'flex';
+        headBar.classList.add("active");
+
+        //Suppression filtres
+        const filter = document.querySelector('.container-filtres');
+        filter.style.display = "none";
+        
+        //Changement text login en logout
+        const logOut = document.getElementById('logout');
+        logOut.innerText ="logout";
+        logOut.href = ""
+
+}
+}
 
   
